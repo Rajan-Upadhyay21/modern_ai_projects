c40b5482c45ac26888f75c73bee7405122a9eba5
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-18 22:28:44 -0500

    Create image_rotation.py

---
+++ opencv_projects/image_rotation.py
@@ -0,0 +1,11 @@
+import cv2
+
+image = cv2.imread("sample.jpg")
+
+if image is None:
+    print("Image not found. Make sure sample.jpg exists.")
+else:
+    rotated = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+    cv2.imshow("Rotated Image", rotated)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
