6da3ed43ed94531030b2da26ac018a42007c54d5
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-20 15:24:53 -0500

    Create adversarial_example_concept.py

---
+++ ml_security_/adversarial_example_concept.py
@@ -0,0 +1,11 @@
+original_input = [0.9, 0.1, 0.2, 0.8]
+perturbed_input = [0.91, 0.09, 0.21, 0.79]
+
+print("Original Input:")
+print(original_input)
+
+print("\nPerturbed Input:")
+print(perturbed_input)
+
+print("\nConcept:")
+print("Small changes in input may cause unexpected model behavior in adversarial settings.")
