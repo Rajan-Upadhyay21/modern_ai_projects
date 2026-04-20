592a2531b7427b20528df4a6f7f91c1322aa1078
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-20 15:07:02 -0500

    Create query_rewriting.py

---
+++ rag_projects/query_rewriting.py
@@ -0,0 +1,15 @@
+query = "RAG basics"
+
+rewritten_queries = [
+    query,
+    "retrieval augmented generation basics",
+    "how retrieval improves language model answers",
+    "grounded AI retrieval and generation"
+]
+
+print("Original Query:")
+print(query)
+
+print("\nRewritten Queries:")
+for q in rewritten_queries:
+    print(q)
