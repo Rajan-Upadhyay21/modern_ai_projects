fbf806d6e0166434487f8ba3c83ca87bab661965
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-20 15:38:08 -0500

    Create tpu_strategy_concept.py

---
+++ distributed_training/tpu_strategy_concept.py
@@ -0,0 +1,3 @@
+print("TPU Strategy Concept:")
+print("In TensorFlow, TPU training often uses tf.distribute.TPUStrategy.")
+print("This helps scale training across TPU cores for large models and datasets.")
