e9c66f1fd798585b234661ef2563282ee12ed84c
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-21 16:01:26 -0500

    Create model_version_endpoint.py

---
+++ deployment_projects/model_version_endpoint.py
@@ -0,0 +1,8 @@
+model_metadata = {
+    "model_name": "customer_churn_model",
+    "version": "v1.2.0",
+    "status": "active"
+}
+
+print("Model Version Endpoint Response:")
+print(model_metadata)
