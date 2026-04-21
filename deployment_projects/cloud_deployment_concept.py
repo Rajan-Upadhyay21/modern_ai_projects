4a03cbb4a5b4b6491db0e73558e37ceca8191ecf
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-21 16:02:28 -0500

    Create cloud_deployment_concept.py

---
+++ deployment_projects/cloud_deployment_concept.py
@@ -0,0 +1,11 @@
+cloud_targets = [
+    "AWS EC2",
+    "AWS Lambda",
+    "Google Cloud Run",
+    "Azure App Service",
+    "Docker Container Platform"
+]
+
+print("Cloud Deployment Targets:")
+for target in cloud_targets:
+    print("-", target)
