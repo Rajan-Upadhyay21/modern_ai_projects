4f2b6876cb0d37a21d06474bbf351b4a3063489b
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-21 15:51:22 -0500

    Create cold_start_concept.py

---
+++ recommendation_projects/cold_start_concept.py
@@ -0,0 +1,16 @@
+new_user = {
+    "user_id": "User5",
+    "history": [],
+    "preferred_genre": "Action"
+}
+
+popular_items = ["Movie_A", "Movie_B", "Movie_D"]
+
+print("Cold Start User:")
+print(new_user)
+
+print("\nSuggested Strategy:")
+print("Use popularity-based or content-based recommendations first.")
+
+print("\nPopular Items to Recommend:")
+print(popular_items)
