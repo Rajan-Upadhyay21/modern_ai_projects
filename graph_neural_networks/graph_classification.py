595d320a868dd73a29de8c1aa41be35376338b2a
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-21 15:45:04 -0500

    Create graph_classification.py

---
+++ graph_neural_networks/graph_classification.py
@@ -0,0 +1,9 @@
+graphs = [
+    {"graph_id": 1, "num_nodes": 4, "num_edges": 3, "label": "social"},
+    {"graph_id": 2, "num_nodes": 6, "num_edges": 5, "label": "molecule"},
+    {"graph_id": 3, "num_nodes": 5, "num_edges": 4, "label": "social"}
+]
+
+print("Graph Classification Data:")
+for graph in graphs:
+    print(graph)
