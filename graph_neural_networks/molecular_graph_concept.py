151571cf2bf6742ee7b9d78cbc4827a9f17ebd70
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-21 15:46:52 -0500

    Create molecular_graph_concept.py

---
+++ graph_neural_networks/molecular_graph_concept.py
@@ -0,0 +1,8 @@
+molecule = {
+    "atoms": ["C", "O", "H", "H"],
+    "bonds": [("C", "O"), ("C", "H"), ("C", "H")]
+}
+
+print("Molecular Graph Concept:")
+print("Atoms:", molecule["atoms"])
+print("Bonds:", molecule["bonds"])
