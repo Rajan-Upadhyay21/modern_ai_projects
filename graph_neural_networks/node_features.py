e6bac6413a77c3d2a91d0e093ccd02e4b6ff2205
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-21 15:44:16 -0500

    Create node_features.py

---
+++ graph_neural_networks/node_features.py
@@ -0,0 +1,10 @@
+node_features = {
+    "A": [1.0, 0.5, 0.2],
+    "B": [0.9, 0.1, 0.8],
+    "C": [0.3, 0.7, 0.4],
+    "D": [0.6, 0.2, 0.9]
+}
+
+print("Node Features:")
+for node, features in node_features.items():
+    print(f"{node} -> {features}")
