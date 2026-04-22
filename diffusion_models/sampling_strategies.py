12bcdec22e0a9f8993aef89882bee2dd8fbfceca
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-22 13:51:19 -0500

    Create sampling_strategies.py

---
+++ diffusion_models/sampling_strategies.py
@@ -0,0 +1,10 @@
+sampling_methods = [
+    "DDPM sampling",
+    "DDIM sampling",
+    "Ancestral sampling",
+    "Deterministic accelerated sampling"
+]
+
+print("Diffusion Sampling Strategies:")
+for method in sampling_methods:
+    print("-", method)
