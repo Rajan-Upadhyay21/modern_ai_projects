4ada9eea25f856c652d54a7be3f8b1485025874d
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-22 13:50:27 -0500

    Create latent_diffusion_concept.py

---
+++ diffusion_models/latent_diffusion_concept.py
@@ -0,0 +1,8 @@
+image_space = "High-dimensional pixel space"
+latent_space = "Compressed representation space"
+benefit = "More efficient training and sampling"
+
+print("Latent Diffusion Concept:")
+print("Image Space:", image_space)
+print("Latent Space:", latent_space)
+print("Benefit:", benefit)
