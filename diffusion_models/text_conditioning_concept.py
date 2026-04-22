9f7b1d90180b761836de7f8219e548ba1843929e
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-22 13:51:01 -0500

    Create text_conditioning_concept.py

---
+++ diffusion_models/text_conditioning_concept.py
@@ -0,0 +1,13 @@
+text_prompt = "A futuristic city skyline at sunset"
+
+text_embedding = [0.82, 0.14, 0.33, 0.57]
+image_generation_condition = "Use text embedding to guide denoising"
+
+print("Text Prompt:")
+print(text_prompt)
+
+print("\nText Embedding:")
+print(text_embedding)
+
+print("\nConditioning Logic:")
+print(image_generation_condition)
