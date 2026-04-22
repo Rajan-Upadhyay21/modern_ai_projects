befeb00f793a78b5a49757b5f24a533f4ded5af2
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-22 14:02:55 -0500

    Create dashboard_metrics_concept.py

---
+++ observability_for_ai/dashboard_metrics_concept.py
@@ -0,0 +1,12 @@
+dashboard_metrics = [
+    "request_volume",
+    "average_latency",
+    "error_rate",
+    "prediction_distribution",
+    "feature_drift_score",
+    "model_health_status"
+]
+
+print("Dashboard Metrics Concept:")
+for metric in dashboard_metrics:
+    print("-", metric)
