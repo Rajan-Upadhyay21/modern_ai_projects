2036ee51955f36525ae32a15986b14f4cd75952b
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-22 13:43:59 -0500

    Create query_expansion.py

---
+++ search_systems/query_expansion.py
@@ -0,0 +1,15 @@
+query = "search system"
+
+expanded_queries = [
+    query,
+    "information retrieval system",
+    "document search ranking",
+    "semantic and lexical retrieval"
+]
+
+print("Original Query:")
+print(query)
+
+print("\nExpanded Queries:")
+for item in expanded_queries:
+    print(item)
