df5a8e30c6ddbd3a2685d79e9e430a2cf5c98972
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-22 13:56:42 -0500

    Create feature_monitoring.py

---
+++ feature_store_projects/feature_monitoring.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+feature_values = np.array([120.5, 125.0, 119.8, 122.1, 121.4, 220.0])
+
+print("Feature Monitoring Statistics:")
+print("Mean:", feature_values.mean())
+print("Min:", feature_values.min())
+print("Max:", feature_values.max())
+print("Std:", feature_values.std())
