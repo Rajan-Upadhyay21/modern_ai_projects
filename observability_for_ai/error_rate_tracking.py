6b28749b3e3dc6b11c2a6cf6e0662bccf7b56e2f
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-22 14:01:40 -0500

    Create error_rate_tracking.py

---
+++ observability_for_ai/error_rate_tracking.py
@@ -0,0 +1,9 @@
+requests = 1000
+errors = 27
+
+error_rate = errors / requests
+
+print("Error Rate Tracking:")
+print("Total Requests:", requests)
+print("Errors:", errors)
+print("Error Rate:", error_rate)
