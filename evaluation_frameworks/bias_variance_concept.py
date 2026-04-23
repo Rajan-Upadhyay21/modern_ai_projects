a68f904d7c7d327935a7cf05bb8c5673bd04190a
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-23 11:23:04 -0500

    Create bias_variance_concept.py

---
+++ evaluation_frameworks/bias_variance_concept.py
@@ -0,0 +1,13 @@
+high_bias_model = {
+    "training_error": 0.20,
+    "validation_error": 0.22
+}
+
+high_variance_model = {
+    "training_error": 0.05,
+    "validation_error": 0.25
+}
+
+print("Bias-Variance Concept:")
+print("High Bias Model:", high_bias_model)
+print("High Variance Model:", high_variance_model)
