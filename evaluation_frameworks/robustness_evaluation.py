92137d4b75ba724f5b153593ddeb7cee70e1a1d4
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-23 11:23:53 -0500

    Create robustness_evaluation.py

---
+++ evaluation_frameworks/robustness_evaluation.py
@@ -0,0 +1,8 @@
+original_accuracy = 0.92
+noisy_input_accuracy = 0.81
+shifted_distribution_accuracy = 0.76
+
+print("Robustness Evaluation:")
+print("Original Accuracy:", original_accuracy)
+print("Noisy Input Accuracy:", noisy_input_accuracy)
+print("Shifted Distribution Accuracy:", shifted_distribution_accuracy)
