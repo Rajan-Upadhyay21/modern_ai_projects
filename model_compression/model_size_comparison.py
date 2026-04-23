0bbe5562f403534a606e64d9810603eec3db144d
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-23 11:51:28 -0500

    Create model_size_comparison.py

---
+++ model_compression/model_size_comparison.py
@@ -0,0 +1,8 @@
+models = [
+    {"name": "teacher_model", "size_mb": 250},
+    {"name": "compressed_model", "size_mb": 65}
+]
+
+print("Model Size Comparison:")
+for model in models:
+    print(model)
