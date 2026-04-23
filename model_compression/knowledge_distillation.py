87f449b0c93a753da825e132ad105078d0d323de
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-23 11:50:55 -0500

    Create knowledge_distillation.py

---
+++ model_compression/knowledge_distillation.py
@@ -0,0 +1,7 @@
+teacher_accuracy = 0.94
+student_accuracy = 0.90
+
+print("Knowledge Distillation:")
+print("Teacher Model Accuracy:", teacher_accuracy)
+print("Student Model Accuracy:", student_accuracy)
+print("Student learns to imitate teacher behavior while being smaller.")
