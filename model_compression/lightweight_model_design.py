69374b8a1aaf23ce7cc22f06b57a0f69af60f5a9
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-23 11:52:36 -0500

    Create lightweight_model_design.py

---
+++ model_compression/lightweight_model_design.py
@@ -0,0 +1,11 @@
+design_choices = [
+    "reduce number of layers",
+    "use depthwise separable convolutions",
+    "use smaller hidden dimensions",
+    "prefer efficient activations",
+    "optimize for low memory footprint"
+]
+
+print("Lightweight Model Design Choices:")
+for choice in design_choices:
+    print("-", choice)
